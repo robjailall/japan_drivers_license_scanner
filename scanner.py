7f4554cdb45198f12a1582c51d56274d44c9fa52
Robert Jailall <rob.jailall@gmail.com>
2026-01-16 22:29:59 +0900

    Add debug

--- scanner.py
+++ scanner.py
@@ -32,6 +32,8 @@ def run_booking_flow():
         target_text = "【免許手続】29の国・地域以外で取得した運転免許証を日本の免許に切り替える方(Applicants for license conversion)"
         # This is for INSIDE the 29 countries
         # target_text = "【免許手続】29の国・地域で取得した運転免許証を日本の免許に切り替える方(Applicants for license conversion)"
+
+        print("Looking on the page for: {}".format(target_text))
         page.get_by_text(target_text).click()
 
         print("--- ステップ3: 空き枠（緑の丸）を検索 ---")
